8814ce21633686ee59e90c39442512580436ef9e
Leo Boos <leomb_punk@hotmail.com>
2022-06-09 23:54:31 -0300

    Nombre mal escrito

--- MySQL 10/MyySQL 10.10.sql
+++
@@ -1,5 +0,0 @@
-/*Subconsulta
-10.10. Informar los autores que hayan escrito varios géneros de títulos. Mostrar nombre y 
-cantidad de géneros ordenados por esta última columna empezando por el mayor.*/
-use editorial;
-
